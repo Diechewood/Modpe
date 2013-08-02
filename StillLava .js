ef220ebd48290f86adec4a666e6d877b7eb53cda
Diechewood <elian721@gmail.com>
2013-08-02 03:21:46 -0500

    Update StillLava .js

--- StillLava .js
+++ StillLava .js
@@ -1,9 +1,15 @@
 function useItem(x,y,z,itemId,blockId)
 {
-        if(itemId==280)//If your item is a stick
-        {
-                addItemInventory(11,64);//Adds Still Lava.
+        if(itemId==325)//If your item is a Bucket
+        {                addItemInventory(11,64);//Add 5 Dirt blocks to your inventory.
                 print("HAY WORLD :D");
+        }       
+}
+function attackHook(attacker, victim)
+{
+        if(getCarriedItem()==280)//If your item is a stick
+        {
+                preventDefault();//Don't call the real attack function after
+                setVelY(victim,1.5);//Make them fly vertically =)
         }
-        
 }
