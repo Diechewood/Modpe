78a8f217fa60b0e438e1566708887682f7618bd4
Diechewood <elian721@gmail.com>
2013-08-02 10:15:51 -0500

    Create saddle.js

---
+++ saddle.js
@@ -0,0 +1,7 @@
+function useItem(x,y,z,itemId,blockId)
+{
+        if(itemId==325)//If your item is a Bucket
+        {                addItemInventory(11,64);//Add 5 Dirt blocks to your inventory.
+                print("HAY WORLD :D");
+        }       
+}
