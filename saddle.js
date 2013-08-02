4661fec632d99ae0ee2b3bcc546e8fd856d5c0cb
Diechewood <elian721@gmail.com>
2013-08-02 10:18:41 -0500

    Update saddle.js

--- saddle.js
+++ saddle.js
@@ -1,7 +1,7 @@
 function useItem(x,y,z,itemId,blockId)
 {
         if(itemId==280)//If your item is a Bucket
-        {                addItemInventory(11,64);//Add 5 Dirt blocks to your inventory.
+        {                addItemInventory(329,64);//Add 5 Dirt blocks to your inventory.
                 print("HAY WORLD :D");
         }       
 }
