3a9d6f2844f960d4a358e1bfdd49f1e89ec2ad1f
Diechewood <elian721@gmail.com>
2013-08-02 08:58:38 -0500

    Create explosion.js

---
+++ explosion.js
@@ -0,0 +1,7 @@
+function useItem(x,y,z,itemId,blockId)
+{
+        if(itemId==292&&blockId==4)//If your item is a iron hoe, and the block your using it on is stone
+        {
+                explode(x,y,z,3.1);//Blow it up!
+                
+        }
