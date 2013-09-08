c5d5647f934813d26567e200e22fe94e84727c59
Diechewood <elian721@gmail.com>
2013-09-08 11:56:47 -0500

    Update Test.js

--- Test.js
+++ Test.js
@@ -2,7 +2,7 @@ function useItem(x,y,z,itemId,blockId,side)
 {
         if(blockid==87)//Tap Nether Rack
         {
-                spawnPigZombie(x,y,z,276,Herobrine.png);
+                spawnPigZombie(x, y, z, 276, "mob/Herobrine.png");
                 clientMessage("eiD lliW ouY");
                 setNightMode(isNight); 
         }
